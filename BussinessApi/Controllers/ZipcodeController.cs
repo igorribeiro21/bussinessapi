b94d64b035200bb1e39a7a22f557195127a7544e
igor.ribeiro <igor.ribeiro@consorciomagalu.com.br>
2023-01-07 12:20:33 -0300

    feat-zipcode - feat: create endpoint to get address with zipcode

---
+++ BussinessApi/Controllers/ZipcodeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BussinessApi.Models;
+using Newtonsoft.Json;
+
+namespace BussinessApi.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class ZipcodeController : ControllerBase
+    {
+        private HttpClient _client;
+
+        public ZipcodeController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://viacep.com.br/ws/");
+        }
+
+        [HttpGet("{zipcode}")]
+        public async Task<IActionResult> GetZipcode(string zipcode)
+        {
+            try
+            {
+                var result = await _client.GetAsync($"{zipcode}/json");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var responseBody = await result.Content.ReadAsStringAsync();
+                    ResponseZipcodeModel response = JsonConvert.DeserializeObject<ResponseZipcodeModel>(responseBody);
+
+                    if (response != null)
+                    {
+                        return Ok(response);
+                    }
+                }
+                return BadRequest(new
+                {
+                    message = "Não foi encontrado informações com o cep informado"
+                });
+            }catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    message = ex.Message
+                });
+            }            
+        }
+    }
+}
