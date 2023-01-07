1db10ac24727366ab021bd31b40e78824561dd16
igor.ribeiro <igor.ribeiro@consorciomagalu.com.br>
2023-01-07 13:00:44 -0300

    feat-employee - feat: create crud JobTitles

--- BussinessApi/Context/AppDbContext.cs
+++ BussinessApi/Context/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace BussinessApi.Context
     public class AppDbContext : DbContext
     {
         public DbSet<Company> Companies { get; set; }
+        public DbSet<JobTitles> JobTitles { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
---
+++ BussinessApi/Models/JobTitles.cs
@@ -0,0 +1,8 @@
+﻿namespace BussinessApi.Models
+{
+    public class JobTitles
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
