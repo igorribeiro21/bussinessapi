d7027ae8a86614bed169a6acb3b92cd7a547ee81
igor.ribeiro <igor.ribeiro@consorciomagalu.com.br>
2023-01-09 11:52:13 -0300

    refactor-companies - refactor: ajduste model Company to post and put

---
+++ BussinessApi/Models/CompanyCreateUpdate.cs
@@ -0,0 +1,15 @@
+﻿namespace BussinessApi.Models
+{
+    public class CompanyCreateUpdate
+    {        
+        public string? Name { get; set; }
+        public int Zipcode { get; set; }
+        public string? Street { get; set; }
+        public int Number { get; set; }
+        public string? Complement { get; set; }
+        public string? Neighborhood { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Telephone { get; set; }        
+    }
+}
