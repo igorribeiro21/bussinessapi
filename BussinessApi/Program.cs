109a65e22f1e62c52d7a8c32009a33332688e2f2
igor.ribeiro <igorapalvesribeiro@gmail.com>
2023-01-10 16:39:34 -0300

    main - ci: add host in cors

--- BussinessApi/Program.cs
+++ BussinessApi/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("CorsApi",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000","https://bussiness-web.herokuapp.com")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
